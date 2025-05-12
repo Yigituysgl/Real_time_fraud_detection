6f0a108f9034f7f34861b3fa8c00ecf8ef0b59c2
Yigituysgl <118074055+Yigituysgl@users.noreply.github.com>
2025-05-12 21:45:58 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import joblib
 import time
 
 
+if "fraud_count" not in st.session_state:
+    st.session_state.fraud_count = 0
+if "normal_count" not in st.session_state:
+    st.session_state.normal_count = 0
 model = joblib.load("fraud_rf_model.pkl")
 
 
@@ -17,6 +21,10 @@ current_index = st.session_state.get('index', 0)
 
 st.title("💳 Real-Time Fraud Detection")
 st.write("Simulating incoming transactions and predicting fraud status.")
+st.subheader("📊 Live Summary")
+col1, col2 = st.columns(2)
+col1.metric("🚨 Fraud Detected", st.session_state.fraud_count)
+col2.metric("✅ Normal Transactions", st.session_state.normal_count)
 
 
 if st.button("Next Transaction"):
@@ -29,6 +37,9 @@ if st.button("Next Transaction"):
         with st.spinner("Processing transaction..."):
             time.sleep(1)  
 
+        risk_score = model.predict_proba([X])[0][1]
+            
+
         
         y_pred = model.predict([X])[0]
 
@@ -38,10 +49,13 @@ if st.button("Next Transaction"):
 
         if y_pred == 1:
             st.error("🚨 **FRAUD DETECTED!**")
+            st.session_state.fraud_count += 1
         else:
             st.success("✅ Not Fraud")
+            st.session_state.normal_count += 1
 
         st.caption(f"Actual Label: {int(y_true)}")
+        st.caption(f"Model confidence (fraud risk): {risk_score:.2%}")
 
         
         st.session_state.index = current_index + 1
