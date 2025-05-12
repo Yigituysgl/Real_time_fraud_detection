45e11f1ee454301a72e496637d23f5ca327a3a82
Yigituysgl <118074055+Yigituysgl@users.noreply.github.com>
2025-05-12 21:16:55 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,34 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import time
+
+
+model = joblib.load("fraud_rf_model.pkl")
+
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("test_data.csv")  
+
+df = load_data()
+current_index = st.session_state.get('index', 0)
+
+
+st.title("💳 Real-Time Fraud Detection")
+st.write("Simulating incoming transactions and predicting fraud status.")
+
+if st.button("Next Transaction"):
+    if current_index < len(df):
+        row = df.iloc[current_index]
+        X = row.drop('Class')  # Drop label
+        y_true = row['Class']
+        y_pred = model.predict([X])[0]
+        st.write(f"**Transaction #{current_index + 1}:**")
+        st.dataframe(pd.DataFrame([X]))
+        st.success("✅ Not Fraud") if y_pred == 0 else st.error("🚨 Fraud Detected!")
+        st.caption(f"Actual Label: {int(y_true)}")
+        st.session_state.index = current_index + 1
+    else:
+        st.info("All transactions simulated.")
 
